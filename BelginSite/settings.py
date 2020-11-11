da2342ca8ec9684824cac427f58b2661a878a0a1
Belgin Moses <belginmoses12@gmail.com>
2020-11-11 11:21:34 +0530

    added env variable

--- BelginSite/settings.py
+++ BelginSite/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')gqx#)v14jh=9ing$4&30q%juqw(zbo=(9gq%wo!m-unxmk+50'
+SECRET_KEY = os.environ.get('DJANGO_PORTFOLIO_APP_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
